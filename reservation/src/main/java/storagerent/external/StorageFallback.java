d775eedf9b1b9c1225d9288f1ed6ac70d165deb1
ByoungGwanLee <lbg@naver.com>
2021-07-07 16:12:36 +0900

    ....

--- reservation/src/main/java/storagerent/external/StorageFallback.java
+++ reservation/src/main/java/storagerent/external/StorageFallback.java
@@ -4,12 +4,14 @@ package storagerent.external;
 import java.text.SimpleDateFormat;
 
 public class StorageFallback{
-    public void chkAndReqReserve(long storageId)
+    public boolean chkAndReqReserve(long storageId)
     {
         SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy년 MM월dd일 HH시mm분ss초");
                 
         String format_time1 = format1.format (System.currentTimeMillis());
 
         System.out.println("chkAndReqReserve Fall Back Called :" + format_time1);
+
+        return false;
     }
 }
