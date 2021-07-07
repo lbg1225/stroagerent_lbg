7f82d8b852c4670ad6b07d259fba788c811a3ff1
lbg1225 <lbg1225@gmail.com>
2021-07-08 08:01:34 +0900

    ....

--- restart.sh
+++ restart.sh
@@ -2,6 +2,8 @@
 kubectl delete svc,deploy,pod -n storagerent --all
 kubectl delete ns storagerent
 kubectl create ns storagerent
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl get deployment metrics-server -n kube-system
 kubectl apply -f yml/kafkapod.yml
 kubectl apply -f ./yml/siege.yml
 kubectl apply -f yml/configmap.yml
