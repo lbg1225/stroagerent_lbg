6552440205da1e4f9886d5b72c9f3a646184e3c5
lbg1225 <lbg1225@gmail.com>
2021-07-08 09:59:57 +0900

    ....

--- restart.sh
+++ restart.sh
@@ -8,8 +8,8 @@ kubectl apply -f yml/kafkapod.yml
 kubectl apply -f ./yml/siege.yml
 kubectl apply -f yml/configmap.yml
 ./kubedeploy.sh v1
-./storage_autoscale.sh v1
-kubectl autoscale deploy storage -n storagerent --min=1 --max=10 --cpu-percent=15
-kubectl get all -n storagerent
-kubectl get deploy storage -w -n storagerent
+#./storage_autoscale.sh v1
+#kubectl autoscale deploy storage -n storagerent --min=1 --max=10 --cpu-percent=15
+#kubectl get all -n storagerent
+#kubectl get deploy storage -w -n storagerent
 
